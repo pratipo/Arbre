6b98b206e4c049ff3a2eb7fbcfe482896bd39ca2
Andrea <elmacdealicia@MacBook-Pro-de-Alicia-Sanchez-Garcia.local>
2020-05-22 21:17:49 +0200

    + func dead, timer, group areas 2d,...

--- Tree_d21/Script/Player_R.gd
+++ Tree_d21/Script/Player_R.gd
@@ -1,23 +1,35 @@
 extends Node2D
 
 onready var Player_R = $Player_R
+onready var Dead = $Dead
+onready var Taimer = $Timer
 
 var speed = 230
 var direction = -1
 
 
 func _ready():
-	pass # Replace with function body.
+	connect("area_entered", self, "_on_area_entered")
 
+func _on_area_entered(area):
+	if area.is_in_group("Chof"):
+		Player_R.dead()
 
 #func initialise_Player_R():
 	#pass
 	
+func _process(delta):
+	position.x += speed * direction * delta
+
 func remove (from_left):
 		if from_left:
 			direction = -1
 		else:
 			direction = 1
 
-func _process(delta):
-	position.x += speed * direction * delta
+func dead ():
+	Dead.position.x = Player_R.position.x
+	Dead.visible = true
+	 
+func _on_Timer_timeout():
+	pass # Replace with function body.
